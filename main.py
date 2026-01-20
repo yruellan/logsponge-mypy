917e34a87704cae772a18220aad22c79a92bc5de
Yann <ruellan.yann@laposte.net>
2026-01-20 15:36:40 +0100

    Remove unused files

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from logicsponge!")
-
-
-if __name__ == "__main__":
-    main()
--- mypy_pkg/ls_lib.py
+++
@@ -1,8 +0,0 @@
-from mypy.plugin import AnalyzeTypeContext
-from mypy.types import AnyType, TypeOfAny, TupleType, Type
-import logicsponge.core as ls
-
-def analyze_vector(ctx: AnalyzeTypeContext) -> Type:
-    print(f"[plugin] analyze_vector called with {ctx.type = } {ctx.context = }")
-    
-    return AnyType(TypeOfAny.special_form)  # Placeholder implementation
